82ede117116f1719ffa71ac2756ce3fd4a058104
romeoliveirasantos <rjob.oliveira@gmail.com>
2025-03-05 08:38:43 -0300

    Sobre

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import menu from './menu.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div className="sobre">
+          <div>SOBRE</div>
+          <div className='Sobre-menu'>
+            <img src={menu} className='Menu-icon' alt='Menu icon'/>
+          </div>
+        </div>
         <p>
-          Edit <code>src/App.js</code> e salve para reload.
+          Edite o <code>src/App.js</code> e salve para reload.
         </p>
         <a
           className="App-link"
